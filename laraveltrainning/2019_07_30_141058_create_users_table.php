411417053af5d4101be0e5d938efe6bca13c5e32
cuong dang <cuongdq@haposoft.com>
2019-08-01 03:00:59 +0700

    31/07/2019

---
+++ laraveltrainning/2019_07_30_141058_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->timestamps();
+            $table->varchar('first_name');
+            $table->varchar('last_name');
+            $table->varchar('email');
+            $table->varchar('job_title');
+            $table->varchar('city');
+            $table->varchar('country');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
